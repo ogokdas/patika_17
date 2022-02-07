bd4c8cc35a3a76c4d960e494ab7a8533dbb9d63a
ogokdas <ogokdas@yandex.com>
2022-02-07 23:32:01 +0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace patika_17
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string birinci = "Patika dev yazılım şirketi";
+            string ikinci = "Patika dev yazılım şirketi";
+
+            Console.WriteLine(birinci.Length);
+            Console.WriteLine(birinci.CompareTo(ikinci)); // 1, -1, 0
+
+            Console.WriteLine(birinci.ToUpper());
+            Console.WriteLine(ikinci.ToLower());
+
+            Console.WriteLine(birinci.Remove(1,2)); // pika
+            Console.WriteLine(ikinci.PadRight(50, '-'));
+
+            string [] dizi = ikinci.Split();
+            foreach (var item in dizi)
+            {
+                Console.WriteLine(item);
+            }
+            
+
+        }
+    }
+}
